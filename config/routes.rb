b5e3fe93f4c58a4711c094afbd4a7717eb0adddb
debian <jhang0912407249@gmail.com>
2020-09-19 10:35:11 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "todos#index"
-  get "/0", to: "todos#test0"
-  get "1", to: "todos#test1"
-  get "2", to: "todos#test2"
-  get "3", to: "todos#test3"
-  get "4", to: "todos#test4"
-  get "5", to: "todos#test5"
-  get "6", to: "todos#test6"
-  get "7", to: "todos#test7"
-  get "8", to: "todos#test8"
+  get "/0", to: "shooly#privacy"
+  get "/1", to: "shooly#order_information"
+  get "/2", to: "shooly#service_home_wash"
+  get "/3", to: "shooly#service_type"
+  get "/4", to: "todos#test4"
+  get "/5", to: "todos#test5"
+  get "/6", to: "todos#test6"
+  get "/7", to: "todos#test7"
+  get "/8", to: "todos#test8"
 end
