c9d931ef222a838959ca2bb15bc2887b99c56654
debian <jhang0912407249@gmail.com>
2020-09-20 08:58:40 -0400

    add class_eval

--- config/initializers/evenparse.rb
+++ config/initializers/evenparse.rb
@@ -8,8 +8,7 @@ Kamigo::EventParsers::LineEventParser.class_eval do
     line_event.source_group_id = payload.dig(:source, :groupId) || payload.dig(:source, :roomId) || payload.dig(:source, :userId)
     line_event.source_user_id = payload.dig(:source, :userId) || payload.dig(:source, :groupId) || payload.dig(:source, :roomId)
     line_event.message_type = payload.dig(:message, :type) || payload.dig(:type)
-    line_event.time = payload.dig(:postback, :params)
-    line_event.message = payload.dig(:message, :text) || payload.dig(:postback, :data) || payload.dig(:message, :address) || line_event.message_type
+    line_event.message = payload.dig(:message, :text) || payload.dig(:postback, :params) || payload.dig(:message, :address) || line_event.message_type
     line_event
   end
 end
