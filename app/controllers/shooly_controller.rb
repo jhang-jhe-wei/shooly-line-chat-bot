e097828ae38e30a71dce847e377d6dd0f53ee9de
debian <jhang0912407249@gmail.com>
2020-09-20 12:53:00 -0400

    test

--- app/controllers/shooly_controller.rb
+++ app/controllers/shooly_controller.rb
@@ -15,6 +15,7 @@ class ShoolyController < ApplicationController
   end
 
   def order
+    line.push_message(params[:source_user_id], { "type": "text", "text": "訂單已建立！您預約的技師約在25分後到達!" })
   end
 
   def other
