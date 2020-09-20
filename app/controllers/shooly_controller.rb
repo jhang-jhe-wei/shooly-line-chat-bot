b2e7c68bd47b0eb6aee4e6efbb13b162fe25ba92
debian <jhang0912407249@gmail.com>
2020-09-20 12:54:53 -0400

    test

--- app/controllers/shooly_controller.rb
+++ app/controllers/shooly_controller.rb
@@ -112,4 +112,11 @@ class ShoolyController < ApplicationController
     puts "=== kamigo debug info end ==="
     puts ""
   end
+
+  def line
+    @line ||= Line::Bot::Client.new { |config|
+      config.channel_secret = "d90f297390fb8b1769e3e625ccf56828"
+      config.channel_token = "5T+wLKjPrwD5aeANK/A0dM9Z/+QS5Cw1K95jUoKkDefEWY3DtEwI57cX+nHqDtiP7PyMlH/i45lXG6fqdpeEFSXEkKYOS6RJi9fP1/9VEnA7N9IGbb6Xoz7peYyPONK9mco3XectB5C+MLDsag2/jAdB04t89/1O/w1cDnyilFU="
+    }
+  end
 end
