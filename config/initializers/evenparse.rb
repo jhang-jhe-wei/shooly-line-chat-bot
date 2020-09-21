6ef81823179e101690c58478a770d016ea01ac1a
debian <jhang0912407249@gmail.com>
2020-09-20 22:19:09 -0400

    test

--- config/initializers/evenparse.rb
+++ config/initializers/evenparse.rb
@@ -16,7 +16,7 @@ Kamigo::EventParsers::LineEventParser.class_eval do
     payload = JSON.parse(event.to_json, symbolize_names: true)[:src]
     line_event = Kamigo::Events::LineEvent.new
     line_event.payload = payload
-    line_event.reply_token = event["replyToken"]
+    line_event.reply_token = params["events"][0]["replyToken"]
     line_event.source_type = payload.dig(:source, :type)
     line_event.source_group_id = payload.dig(:source, :groupId) || payload.dig(:source, :roomId) || payload.dig(:source, :userId)
     line_event.source_user_id = payload.dig(:source, :userId) || payload.dig(:source, :groupId) || payload.dig(:source, :roomId)
