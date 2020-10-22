eff5e80e8af0cf8ff41dc160aa1fcb29d317d91d
jhang-jhe-wei <jhang0912407249@gmailc.om>
2020-10-22 21:35:15 +0800

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #resources :todos
+  resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "todos#index"
   get "我同意Shooly條款", to: "shooly#accept"
