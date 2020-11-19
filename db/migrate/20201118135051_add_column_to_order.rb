e9c5e58494119c13183b1f00692172176390ab12
ubuntu <jhang0912407249@gmail.com>
2020-11-19 06:12:31 -0800

    test

--- db/migrate/20201118135051_add_column_to_order.rb
+++ db/migrate/20201118135051_add_column_to_order.rb
@@ -1,7 +1,7 @@
 class AddColumnToOrder < ActiveRecord::Migration[5.2]
   def change
     add_column :orders, :name, :string
-    add_column :orders, :time, :dateTime
+    add_column :orders, :time, :datetime
     add_reference :orders, :Technician, foreign_key: true
     add_column :orders, :phone, :string
   end
